f97128134ebee79deb1d38840b230860fc47a6a1
Mikołaj Chudy <mikolajchudy@MacBook-Pro-Mikoaj.local>
2019-05-31 17:44:17 +0200

    Poprawa Styli
    - Poprawa rwd Pierwszej strony
    - poprawa i przeniesienie fontów do abcLayout.blade.php

--- resources/views/layouts/abcLayout.blade.php
+++ resources/views/layouts/abcLayout.blade.php
@@ -3,6 +3,46 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
   <head>
     @include('teamplate.header')
+
+    <style>
+      @font-face {
+        font-family: 'Myriad Pro';
+        src: url('{{asset('/fonts/MyriadPro-Bold.eot')}}');
+        src: url('{{asset('/fonts/MyriadPro-Bold.eot?#iefix')}}') format('embedded-opentype'),
+            url( '{{asset('/fonts/MyriadPro-Bold.woff')}}') format('woff');
+        font-weight: bold;
+        font-style: normal;
+      }
+
+      @font-face {
+        font-family: 'Myriad Pro';
+
+        src: url('{{asset('/fonts/MyriadPro-Semibold.eot')}}');
+        src: url('{asset('/fonts/MyriadPro-Semibold.eot?#iefix')}}') format('embedded-opentype'),
+        url('{asset('/fonts/MyriadPro-Semibold.woff')}}')format('woff');
+        font-weight: 600;
+        font-style: normal;
+      }
+
+      @font-face {
+        font-family: 'Myriad Pro';
+        src: url('{{asset('/fonts/MyriadPro-Regular.eot')}}');
+        src: url('{{asset('/fonts/MyriadPro-Regular.eot?#iefix')}}') format('embedded-opentype'),
+            url('{{asset('/fonts/MyriadPro-Regular.woff')}}') format('woff');
+        font-weight: normal;
+        font-style: normal;
+      }
+
+      @font-face {
+        font-family: 'Museo 900';
+        src: url('{{asset('/fonts/Museo900-Regular.eot')}}');
+        src: url('{{asset('/fonts/Museo900-Regular.eot?#iefix')}}') format('embedded-opentype'),
+            url('{{asset('/fonts/Museo900-Regular.woff')}}') format('woff');
+        font-weight: 900;
+        font-style: normal;
+      }
+
+    </style>
   </head>
 
   <body class="abcLayout-register">
